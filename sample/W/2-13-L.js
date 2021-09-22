9476f3bce0eacf3f24b4c4f0cba483a00cdadaef
thinkingncoding <thinkingncoding@gmail.com>
2021-09-22 15:09:53 +0900

    2-2

---
+++ sample/W/2-13-L.js
@@ -0,0 +1,13 @@
+let number = 10;
+console.log(number); //10
+console.log(++number); //11
+console.log(--number); //10
+console.log(number); //10
+
+console.log("_____________________");
+
+let number1 = 10;
+console.log(number1); //10
+console.log(number1++); //10
+console.log(number1--); //11
+console.log(number1); //10
